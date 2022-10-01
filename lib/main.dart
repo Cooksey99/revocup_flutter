c0cdb9312ba2e712f499a05d19e41fdfd1834729
Andrew Cooksey <andrewcooksey@Andrews-MacBook-Pro.local>
2022-10-01 11:37:26 -0500

    modified files to separate larger widgets to their own files

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:revocup_flutter/orderPage/orderPage.dart';
+
 
 
 void main() {
@@ -91,111 +93,20 @@ class Navbar extends StatelessWidget {
     return DefaultTabController (
       length: navbarOptions.length,
       child: Scaffold(
+        backgroundColor: Colors.brown,
         appBar: AppBar (
           title: const Center(
               child: Text("Revocup Coffee")
           ),
-          backgroundColor: themeColor,
+          backgroundColor: Colors.red,
           bottom: TabBar(
             tabs: navbarTab,
           ),
         ),
-        body: MySlider(),
+        body: const MySlider(),
       )
     );
   }
 }
 
-class MySlider extends StatelessWidget {
-  const MySlider({super.key});
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          CarouselSlider(
-
-            items: [
-
-              //1st Image of Slider
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: NetworkImage("https://th.bing.com/th/id/OIP.dGfYmE04X6VK1WwnHeDBMQHaE7?pid=ImgDet&w=640&h=426&rs=1"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //2nd Image of Slider
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: NetworkImage("https://th.bing.com/th/id/OIP.F2gW9t-PpM6DPLMXZkU_OwHaEo?pid=ImgDet&rs=1"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //3rd Image of Slider
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: NetworkImage("https://images1.persgroep.net/rcs/U-cxKsPBTLkTuTxqB75ry-iRilE/diocontent/148982786/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.9"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //4th Image of Slider
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: NetworkImage("https://th.bing.com/th/id/OIP.aeiuPcAwbqIcTQFeK_BQWQHaE7?pid=ImgDet&w=500&h=333&rs=1"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-              //5th Image of Slider
-              Container(
-                margin: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                    image: NetworkImage("https://th.bing.com/th/id/OIP.T98IV7uqKjMscVaHKpc6ZAHaEK?pid=ImgDet&w=1920&h=1079&rs=1"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-
-            ],
-
-            //Slider Container properties
-            options: CarouselOptions(
-              height: 300.0,
-              enlargeCenterPage: true,
-              autoPlay: true,
-              aspectRatio: 16 / 9,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enableInfiniteScroll: true,
-              autoPlayAnimationDuration: Duration(milliseconds: 600),
-              viewportFraction: 0.8,
-            ),
-          ),
-        ],
-      ),
-
-    );
-  }
-}
---
+++ lib/orderPage/mySlider.dart
@@ -0,0 +1,101 @@
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MySlider());
+}
+
+class MySlider extends StatelessWidget {
+  const MySlider({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          CarouselSlider(
+
+            items: [
+
+              //1st Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://th.bing.com/th/id/OIP.dGfYmE04X6VK1WwnHeDBMQHaE7?pid=ImgDet&w=640&h=426&rs=1"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //2nd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://th.bing.com/th/id/OIP.F2gW9t-PpM6DPLMXZkU_OwHaEo?pid=ImgDet&rs=1"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //3rd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://images1.persgroep.net/rcs/U-cxKsPBTLkTuTxqB75ry-iRilE/diocontent/148982786/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.9"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //4th Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://th.bing.com/th/id/OIP.aeiuPcAwbqIcTQFeK_BQWQHaE7?pid=ImgDet&w=500&h=333&rs=1"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //5th Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: NetworkImage("https://th.bing.com/th/id/OIP.T98IV7uqKjMscVaHKpc6ZAHaEK?pid=ImgDet&w=1920&h=1079&rs=1"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+            ],
+
+            //Slider Container properties
+            options: CarouselOptions(
+              height: 300.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 600),
+              viewportFraction: 0.8,
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
